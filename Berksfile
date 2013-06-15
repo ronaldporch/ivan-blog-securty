b35eaeb54b170690b768d0bd1bc0e49418a75eab
Ivan Storck <ivanoats@gmail.com>
2013-06-15 08:17:15 -0700

    posts

---
+++ Berksfile
@@ -0,0 +1,5 @@
+site :opscode
+
+cookbook 'build-essential'
+cookbook 'apache2'
+cookbook 'postgresql'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog2::Application.config.secret_token = '8101480f283e383e22fb5331faa54551140038a6ded1059d8168fdfddbd7704ca409a0d53c6d2696afbc8d7c147285be48678620084d38f21bf114f34483dae8'
