34973d89b7bb56074d82117c646bc2303e073abe
Ivan Storck <ivanoats@gmail.com>
2013-06-27 13:06:05 -0700

    home page test

--- test/performance/homepage_test.rb
+++ test/performance/homepage_test.rb
@@ -6,6 +6,20 @@ class HomepageTest < ActionDispatch::PerformanceTest
   # self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory]
   #                          :output => 'tmp/performance', :formats => [:flat] }
 
+  def setup
+    100.times do
+      p = Post.create(
+        title: Faker::Company.catch_phrase,
+        body: Faker::Lorem.paragraphs.join("\n")
+      )
+      100.times do
+        c = p.comments.build( body: Faker::Lorem.paragraphs.join("\n"))
+        r = c.replies.build(  body: Faker::Lorem.paragraphs.join("\n"))
+        p.save!
+      end
+    end
+  end
+
   def test_homepage
     get '/'
   end
