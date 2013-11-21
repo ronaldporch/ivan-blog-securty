9e0eea9bf6f47497c38f4ce05e03a3e639d8cdd7
Ivan Storck <ivanoats@gmail.com>
2013-11-21 15:27:20 -0800

    posts can be published (or not)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130615172522) do
+ActiveRecord::Schema.define(:version => 20131121231737) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20130615172522) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "published"
   end
 
   create_table "replies", :force => true do |t|
